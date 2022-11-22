e916f643fc31de7c136ff44688e6e04112f52528
Bryn <bryne12336@gmail.com>
2022-11-22 16:03:13 +0000

    login button shows and hides

--- spoons-app/src/App.js
+++ spoons-app/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 import Login from "./login.js";
-import LoginButton from "./loginButton.js";
+import React, { useState } from "react";
 function App() {
+  const [display, setDisplay] = useState(false);
+  const [hide, setHide] = useState(true);
   return (
     <>
-      {" "}
       <div class="App">
         <div id="header">
-          {" "}
           <h1>So you think you've been to Wetherspoons?</h1>
         </div>
-        <LoginButton></LoginButton>
-        <Login id="login"></Login>
+
+        {hide ? (
+          <button
+            id="loginButton"
+            onClick={() => {
+              setDisplay(!display);
+              setHide(!hide);
+            }}
+          >
+            Login to see your list!
+          </button>
+        ) : null}
+
+        {display ? <Login id="login"></Login> : null}
       </div>
     </>
   );
